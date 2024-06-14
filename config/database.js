57846afdb60af6f581f98b38efaf8ca6d768665d
wesleynoordam <wesley-noordam@hotmail.com>
2024-06-14 13:06:01 +0200

    temp: change db configuration to hardcoded

--- config/database.js
+++ config/database.js
@@ -50,12 +50,12 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        host: 'psql-xprtzbv-cms4.postgres.database.azure.com',
+        port: 5432,
+        database: 'postgres',
+        user: 'MyUser',
+        password: 'SuperSecretPassword11!!',
+        ssl: env.bool('DATABASE_SSL', true) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
@@ -85,7 +85,7 @@ module.exports = ({ env }) => {
   return {
     connection: {
       client,
-      ...connections[client],
+      ...connections['postgres'],
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
   };
