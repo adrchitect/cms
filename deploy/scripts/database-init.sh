6b61e8406899df253a723bd3bbaa64824abf68c6
Maarten Louage <mlouage@users.noreply.github.com>
2024-10-24 14:18:07 +0200

    Add database init script (#26)
    * Add database init script
    
    * Add database init script with encoded passwords
    
    * Update docker compose
    
    * Add Database workflow

---
+++ deploy/scripts/database-init.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+# URL encode both passwords for connection strings
+ADMIN_URL_PASSWORD=$(printf %s "$ADMINPASSWORD" | od -An -tx1 | tr ' ' % | tr -d '\n')
+STRAPI_URL_PASSWORD=$(printf %s "$STRAPIPASSWORD" | od -An -tx1 | tr ' ' % | tr -d '\n')
+
+# For SQL, we need to escape single quotes by doubling them
+SQL_ESCAPED_PASSWORD=$(printf %s "$STRAPIPASSWORD" | awk "{gsub(/'/,\"''\"); print}")
+
+# Commands using admin credentials with encoded password
+psql -d "postgresql://$ADMINUSER:$ADMIN_URL_PASSWORD@$SERVER/postgres" -c "CREATE USER $STRAPIUSER WITH PASSWORD '$SQL_ESCAPED_PASSWORD';"
+psql -d "postgresql://$ADMINUSER:$ADMIN_URL_PASSWORD@$SERVER/postgres" -c "GRANT CONNECT ON DATABASE $STRAPIDATABASENAME TO $STRAPIUSER;"
+psql -d "postgresql://$ADMINUSER:$ADMIN_URL_PASSWORD@$SERVER/postgres" -c "GRANT CREATE ON DATABASE $STRAPIDATABASENAME TO $STRAPIUSER;"
+
+# Use the URL encoded Strapi password in the connection string
+psql -d "postgresql://$STRAPIUSER:$STRAPI_URL_PASSWORD@$SERVER/$STRAPIDATABASENAME" -c "CREATE SCHEMA $STRAPIUSER AUTHORIZATION $STRAPIUSER;"
